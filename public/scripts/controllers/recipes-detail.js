64d4ba313c6e3f04582cfe379c9441d9a5a2c99e
anoopmundathan <anoop.mundathan@gmail.com>
2016-12-04 19:49:00 +0000

    Add Edit recipe

--- public/scripts/controllers/recipes-detail.js
+++ public/scripts/controllers/recipes-detail.js
@@ -5,8 +5,13 @@
 	.controller('RecipeDetailController', RecipeDetailController);
 
 	// Controller
-	function RecipeDetailController() {
-		this.detail = "Anoopss";
+	function RecipeDetailController($scope, recipeService) {
+		
+		// Call service to get data
+		recipeService.getRecipes(function(response) {
+			$scope.recipes = response.data;
+			console.log($scope.recipes);
+		});
 	}
 
 })();
