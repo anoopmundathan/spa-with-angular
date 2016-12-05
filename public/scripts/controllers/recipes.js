68ca04a062d32829f74d5209b4297fd21315a09b
anoopmundathan <anoop.mundathan@gmail.com>
2016-12-05 05:24:15 +0000

    Change name of the Add route

--- public/scripts/controllers/recipes.js
+++ public/scripts/controllers/recipes.js
@@ -6,16 +6,21 @@
 
 	// Controller
 	function RecipesController($scope, $location, recipeService) {
-		
+
+		$scope.isEditing = false;
+
+		function startAdding(path) {
+			console.log('hellokk');
+			$location.path(path);
+		}
 		// Call service to get data
 		recipeService.getRecipes(function(response) {
 			$scope.recipes = response.data;
 		});
 
-		$scope.go = function(path) {
-			console.log(path);
-			$location.path(path);
-		}
+		
+		$scope.startAdding = startAdding;
+		
 	}
 
 })();
